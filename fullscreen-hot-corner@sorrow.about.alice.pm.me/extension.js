4277aecd2d305e3e6fb7f54314752cac88161707
soal <soal@soal.red>
2021-09-22 22:26:49 +0300

    Gnome 41 update

--- fullscreen-hot-corner@sorrow.about.alice.pm.me/extension.js
+++ fullscreen-hot-corner@sorrow.about.alice.pm.me/extension.js
@@ -22,7 +22,7 @@ const _origToggleOverview = HotCorner.prototype._toggleOverview;
 
 function _toggleOverview() {
     // adopted from original Gnome Shell code but with fullscreen check removed
-    // location: /js/ui/layout.js:1177
+    // location: /js/ui/layout.js:1189
     if (Main.overview.shouldToggleByCornerOrButton()) {
         Main.overview.toggle();
         if (Main.overview.animationInProgress)
